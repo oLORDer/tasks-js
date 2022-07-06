61e338dd12edb0a9eb6261e04e5c85426ad3c2d9
oLORDer <justsendmeow@gmail.com>
2022-07-06 22:22:25 +0300

    14

--- js/13.js
+++ js/13.js
@@ -1,3 +1,13 @@
-// Написати ф-ю єObjectEmpty, яка перевіряє об'єкт порожній чи ні. Якщо об'єкт порожній, повернути true, інакше false
+// Написати ф-ю ObjectEmpty, яка перевіряє об'єкт порожній чи ні. Якщо об'єкт порожній, повернути true, інакше false
 // isObjectEmpty({}) // true
 // console.log(isObjectEmpty({name: 'user', age: 21})); // false
+
+function isObjectEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+
+  return true;
+}
+
+console.log(isObjectEmpty({})); // true
--- js/14.js
+++ js/14.js
@@ -3,3 +3,21 @@
 // const productsPrice = { apple: 20, orange: 5, cheese: 12, pork: 45, bread: 23 }
 // const orderA = { apple: 5, cheese: 1, bread: 3 }
 // const orderB = { orange: 10, pork: 2, bread: 1 }
+
+const productsPrice = { apple: 20, orange: 5, cheese: 12, pork: 45, bread: 23 };
+const orderA = { apple: 5, cheese: 1, bread: 3 };
+const orderB = { orange: 10, pork: 2, bread: 1 };
+
+function returnPrice(price, order) {
+  let val = 0;
+  for (let key in order) {
+    for (let pk in price) {
+      if (key === pk) {
+        val += order[key] * price[pk];
+      }
+    }
+  }
+  return val;
+}
+
+console.log(returnPrice(productsPrice, orderA));
