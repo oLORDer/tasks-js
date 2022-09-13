264484312d31fcca3ce289949e4bced9fe49d3ff
Andrii Klochkov <lord@MacBook-Pro-Andrii.local>
2022-09-13 23:53:55 +0300

    start(33)

--- js/28.js
+++ js/28.js
@@ -8,75 +8,87 @@
 // </li>
 const inventors = [
   {
-    name: 'Albert',
-    surname: 'Einstein',
+    name: "Albert",
+    surname: "Einstein",
     born: 1879,
     passed: 1955,
   },
   {
-    name: 'Isaac',
-    surname: 'Newton',
+    name: "Isaac",
+    surname: "Newton",
     born: 1643,
     passed: 1727,
   },
   {
-    name: 'Galileo',
-    surname: 'Galilei',
+    name: "Galileo",
+    surname: "Galilei",
     born: 1564,
     passed: 1642,
   },
   {
-    name: 'Marie',
-    surname: 'Curie',
+    name: "Marie",
+    surname: "Curie",
     born: 1867,
     passed: 1934,
   },
   {
-    name: 'Johannes',
-    surname: 'Kepler',
+    name: "Johannes",
+    surname: "Kepler",
     born: 1571,
     passed: 1630,
   },
   {
-    name: 'Nicolaus',
-    surname: 'Copernicus',
+    name: "Nicolaus",
+    surname: "Copernicus",
     born: 1473,
     passed: 1543,
   },
   {
-    name: 'Max',
-    surname: 'Planck',
+    name: "Max",
+    surname: "Planck",
     born: 1858,
     passed: 1947,
   },
   {
-    name: 'Katherine',
-    surname: 'Blodgett',
+    name: "Katherine",
+    surname: "Blodgett",
     born: 1898,
     passed: 1979,
   },
   {
-    name: 'Ada',
-    surname: 'Lovelace',
+    name: "Ada",
+    surname: "Lovelace",
     born: 1815,
     passed: 1852,
   },
   {
-    name: 'Sarah E.',
-    surname: 'Goode',
+    name: "Sarah E.",
+    surname: "Goode",
     born: 1855,
     passed: 1905,
   },
   {
-    name: 'Lise',
-    surname: 'Meitner',
+    name: "Lise",
+    surname: "Meitner",
     born: 1878,
     passed: 1968,
   },
   {
-    name: 'Hanna',
-    surname: 'Hammarström',
+    name: "Hanna",
+    surname: "Hammarström",
     born: 1829,
     passed: 1909,
   },
 ];
+
+const body = document.querySelector("body");
+
+const res = inventors.map(
+  ({ name, surname, born, passed }) => `<li>
+   <p>name: ${name}</p>
+   <p>surname: ${surname}</p>
+   <p>Age: ${passed - born}</p>
+ </li>`
+);
+
+body.insertAdjacentHTML("beforeend", `<ul>${res.join('')}</ul>`);
--- js/33.js
+++ js/33.js
@@ -8,3 +8,27 @@
 //    При натисканні на todo-замітку у списку, вона стає виконаною, тобто. текст закреслюється (клас complete)
 //    Чекбокс при додаванні нотатки знову має стати не вибраним, а кнопка знову недоступною
 //    Додати кнопку Видалити на кожну нотатку (+функціонал видалення)
+
+const form = document.querySelector('form');
+// const input = document.querySelector('#todoInput');
+const checkbox = document.querySelector('#checkbox');
+const btn = document.querySelector('#btn');
+const list = document.querySelector('.list');
+
+checkbox.addEventListener('change', () => {checkbox.checked ? (btn.disabled = false) : (btn.disabled = true)})
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    const formelEments = e.currentTarget.elements;
+    const input = formelEments['todoInput'];
+    // const checkbox = formelEments['checkbox'];
+    // const btn = formelEments['btn'];
+    list.insertAdjacentHTML("afterbegin", `<li>${input.value}</li>`)
+
+    input.value = '';
+    checkbox.checked = false;
+    btn.disabled = true;
+    console.log();
+}
